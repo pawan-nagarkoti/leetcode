ca61b909c3fd43312769f43c79ff84bc65dacfdf
Pawan <pawan@MacBookAir.lan>
2025-11-18 10:47:59 +0530

    binary search tree

--- index.js
+++ index.js
@@ -1254,12 +1254,33 @@
 // console.log(st, count);
 
 /// linear search
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) {
-      return i;
+// function linearSearch(arr, target) {
+//   for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === target) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
+// console.log(linearSearch([2, 3, 4, 5, 10], 2));
+
+// binary search
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === target) {
+      return mid;
+    } else if (target < arr[mid]) {
+      right = mid - 1;
+    }
+    // } else if (target > arr[mid]) {
+    else {
+      left = mid + 1;
     }
   }
   return -1;
 }
-console.log(linearSearch([2, 3, 4, 5, 10], 2));
+console.log(binarySearch([2, 3, 4, 5, 10], 10));
